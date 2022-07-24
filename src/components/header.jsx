cc5cb073b1857499aad361abac320d6f70d55da6
Durand <junior_durand@outlook.com>
2022-07-23 23:12:00 -0300

    fix header session.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,57 @@
-import React from "react";
-import {Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import React, {useState, useEffect} from "react";
+import {Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import {isAuthenticated, logout} from '../Auth'
+import UsuarioService from '../UsuarioService'
+import EgressoService from '../EgressoService'
 
 function Header(){
+
+    const usuarioService = new UsuarioService(); 
+    const egressoService = new EgressoService(); 
+
+    const [egressoId, setEgressoId] = useState();
+    const [adm, setAdm] = useState(false);
+    const [userId, setUserId] = useState(false);
+
+    useEffect( ()=>{
+        usuarioService.usuarioAtual().then( response => {
+            console.log(response.data);
+            setAdm(response.data.adm == 1)
+            setUserId(response.data.id)
+        })
+        egressoService.egressoAtual().then( response => {
+            setEgressoId(response.data.id);
+            console.log(response.data.id);
+        })
+    },[]);
+
+    const sessionBtn = () => {
+        if(isAuthenticated()){
+            if(adm){
+                return (
+                    <React.Fragment>
+                        <Button variant="primary" href="#/administrador" style={{marginRight: 10}}>Minha Conta</Button>
+                        <Button variant="secondary" onClick={logout}>Logout</Button>
+                    </React.Fragment>
+                )
+            }else{
+                return (
+                    <React.Fragment>
+                        <Button variant="primary" href={`#/egresso/${egressoId}`} style={{marginRight: 10}}>Minha Conta</Button>
+                        <Button variant="secondary" onClick={logout}>Logout</Button>
+                    </React.Fragment>
+                )
+            }
+        }else{
+            return (
+                <React.Fragment>
+                    <Nav.Link href="#/cadastro">Cadastrar</Nav.Link>
+                    <Nav.Link href="#/login">Logar</Nav.Link>
+                </React.Fragment>
+            )
+        }
+    }
+
     return(
         <Navbar variant='dark' expand="lg" style={{"backgroundColor": "#813772"}}>
             <Container>
@@ -14,8 +64,7 @@ function Header(){
                         <Nav.Link href="#/depoimentos">Depoimentos</Nav.Link>
                     </Nav>
                     <Nav className="">
-                        <Nav.Link href="#/cadastro">Cadastrar</Nav.Link>
-                        <Nav.Link href="#/login">Logar</Nav.Link>
+                        {sessionBtn()}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
